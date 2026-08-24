e3099bfec85c3ac63abdaf944f9bcb8f591e63e1
koushikmaya04 <koushikmaya04@gmail.com>
2026-08-24 17:43:49 +0530

    Add Week 4 Task 1 prototype inheritance

---
+++ week-04/task-01-inheritance-polymorphism/prototype-inheritance.js
@@ -0,0 +1,30 @@
+// Classical inheritance using prototypes
+
+// Parent
+function Animal(name) {
+    this.name = name;
+}
+
+Animal.prototype.speak = function () {
+    return this.name + " makes a sound";
+};
+
+// Child
+function Dog(name, breed) {
+    Animal.call(this, name);
+    this.breed = breed;
+}
+
+// Inherit Animal methods
+Dog.prototype = Object.create(Animal.prototype);
+
+// Dog's own method
+Dog.prototype.bark = function () {
+    return this.name + " barks";
+};
+
+// Create object
+const dog = new Dog("Bruno", "Labrador");
+
+console.log(dog.speak()); // Bruno makes a sound
+console.log(dog.bark());  // Bruno barks
